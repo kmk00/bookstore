462a950ab9d0f0ab5d1ad4f8d68d1f7f538f6b34
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-06-13 11:15:11 +0200

    Style welcome page

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,24 +1,30 @@
-<nav class=" flex flex-1 justify-end">
+<nav class="flex flex-1 gap-4 mr-4 items-center justify-end">
     @auth
-        <a
-            href="{{ url('/dashboard') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-        >
-            Dashboard
+        <a href="{{ url('/about-us') }}"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]">
+            Store
+        </a>
+        <a href="{{ url('/store') }}"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]">
+            About Us
+        </a>
+        <a class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]" href="{{ url('/card') }}">
+            <img src="{{ asset('images/cart.png') }}" alt="Cart" class="w-6 h-6">
+        </a>
+
+        <a href="{{ url('/dashboard') }}"
+            class="rounded-md px-5 py-2 bg text-white bg-primary ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]">
+            Profile
         </a>
     @else
-        <a
-            href="{{ route('login') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20"
-        >
+        <a href="{{ route('login') }}"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20">
             Log in
         </a>
 
         @if (Route::has('register'))
-            <a
-                href="{{ route('register') }}"
-                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-            >
+            <a href="{{ route('register') }}"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]">
                 Register
             </a>
         @endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,9 +16,190 @@
 </head>
 
 <body class="antialiased font-sans">
-    @if (Route::has('login'))
-        <livewire:welcome.navigation />
-    @endif
+    <header class="bg-gray-100 relative">
+        <img class="absolute top-0 w-full z-0" src="{{ asset('images/bg-wave.svg') }}" alt="" srcset="">
+        <div class="relative z-10 p-2">
+            @if (Route::has('login'))
+                <livewire:welcome.navigation />
+            @endif
+        </div>
+    </header>
+    <div class="relative z-10 mt-20 p-2">
+        <div class="flex flex-col text-center max-w-5xl mx-auto">
+            <p class="text-xl text-primary">
+                Your favorite bookstore
+            </p>
+            <h1 class="text-5xl font-bold">
+                Welcome to our bookstore app
+            </h1>
+        </div>
+        <div class="flex justify-center mt-20 max-w-5xl mx-auto">
+            <form class="border-black border-2 w-full" action="">
+                <div class="flex items-center p-2">
+                    <input class="border-none h-14 text-center w-full" type="text" name="search" id="search"
+                        placeholder="Book's name">
+                    <button class="px-8 bg-primary h-14  text-white" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <main class="relative z-10 mt-40 max-w-7xl mx-auto p-2">
+        <div class="flex justify-between">
+            <h2 class="text-3xl ">
+                All our books
+            </h2>
+            <div class="flex gap-2">
+                <a href="/">
+                    Group 1
+                </a>
+                <a href="/">
+                    Group 1
+                </a>
+                <a href="/">
+                    See all
+                </a>
+            </div>
+        </div>
+
+        <div class="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 p-2">
+            <div>
+                <img src="{{ asset('images/book-cover-placeholder.png') }}" alt="Book">
+
+                <div class="w-fit">
+                    <p class="font-bold">
+                        Book Title
+                    </p>
+
+                    <p>
+                        Author
+                    </p>
+
+                    <p class="font-bold">
+                        Price
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="{{ asset('images/book-cover-placeholder.png') }}" alt="Book">
+
+                <div class="w-fit">
+                    <p class="font-bold">
+                        Book Title
+                    </p>
+
+                    <p>
+                        Author
+                    </p>
+
+                    <p class="font-bold">
+                        Price
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="{{ asset('images/book-cover-placeholder.png') }}" alt="Book">
+
+                <div class="w-fit">
+                    <p class="font-bold">
+                        Book Title
+                    </p>
+
+                    <p>
+                        Author
+                    </p>
+
+                    <p class="font-bold">
+                        Price
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="{{ asset('images/book-cover-placeholder.png') }}" alt="Book">
+
+                <div class="w-fit">
+                    <p class="font-bold">
+                        Book Title
+                    </p>
+
+                    <p>
+                        Author
+                    </p>
+
+                    <p class="font-bold">
+                        Price
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="{{ asset('images/book-cover-placeholder.png') }}" alt="Book">
+
+                <div class="w-fit">
+                    <p class="font-bold">
+                        Book Title
+                    </p>
+
+                    <p>
+                        Author
+                    </p>
+
+                    <p class="font-bold">
+                        Price
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="{{ asset('images/book-cover-placeholder.png') }}" alt="Book">
+
+                <div class="w-fit">
+                    <p class="font-bold">
+                        Book Title
+                    </p>
+
+                    <p>
+                        Author
+                    </p>
+
+                    <p class="font-bold">
+                        Price
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="{{ asset('images/book-cover-placeholder.png') }}" alt="Book">
+
+                <div class="w-fit">
+                    <p class="font-bold">
+                        Book Title
+                    </p>
+
+                    <p>
+                        Author
+                    </p>
+
+                    <p class="font-bold">
+                        Price
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="{{ asset('images/book-cover-placeholder.png') }}" alt="Book">
+
+                <div class="w-fit">
+                    <p class="font-bold">
+                        Book Title
+                    </p>
+
+                    <p>
+                        Author
+                    </p>
+
+                    <p class="font-bold">
+                        Price
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
 
 </body>
 
