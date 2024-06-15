48d490a7b45cb12e556fe0e16a0313e7f89c3d22
kmk00 <73724318+kmk00@users.noreply.github.com>
2024-06-15 15:24:12 +0200

    Update navigation.blade.php

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -11,8 +11,9 @@
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]">
             About Us
         </a>
-        <a class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]" href="{{ url('/cart') }}">
+        <a class="relative rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]" href="{{ url('/cart') }}">
             <img src="{{ asset('images/cart.png') }}" alt="Cart" class="w-6 h-6">
+            <p class="absolute top-0 right-0 bg-primary text-white rounded-full w-5 h-5 text-center">3</p>
         </a>
 
         <a href="{{ url('/dashboard') }}"
